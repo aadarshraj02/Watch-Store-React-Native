a6cee840ad4ba1a18a3629e630fd67ca4afc6c69
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-12 20:28:25 +0530

    added conditional rendering of underline

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,18 +1,38 @@
 import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {fontSize, spacing} from '../constants/dimensions';
 import {fontFamily} from '../constants/fonts';
 import {colors} from '../constants/colors';
 import {category} from '../data/data';
 
 const Category = (): JSX.Element => {
+  const [selectedCategory, setSelectedCategory] =
+    useState<string>('Smart Watch');
+
+  const handleSelectCategory = (category: string) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <FlatList
       data={category}
       renderItem={({item}) => (
-        <TouchableOpacity>
-          <Text style={styles.categoryText}>{item.name}</Text>
-          <View style={styles.underline}></View>
+        <TouchableOpacity
+          onPress={() => {
+            handleSelectCategory(item.name);
+          }}>
+          <Text
+            style={[
+              styles.categoryText,
+              selectedCategory === item.name && {
+                color: colors.purple,
+              },
+            ]}>
+            {item.name}
+          </Text>
+          {selectedCategory === item.name && (
+            <View style={styles.underline}></View>
+          )}
         </TouchableOpacity>
       )}
       keyExtractor={item => item.id}
