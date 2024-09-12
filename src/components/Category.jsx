c4b7484be6079484dfe6479142e53775767cf84b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-12 19:29:10 +0530

    added header text for category and styled header

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,14 +1,23 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import {fontSize} from '../constants/dimensions';
+import {fontFamily} from '../constants/fonts';
+import {colors} from '../constants/colors';
 
 const Category = () => {
   return (
-    <View>
-      <Text>Category</Text>
-    </View>
+    <TouchableOpacity>
+      <Text style={styles.categoryText}>Smart Watch</Text>
+    </TouchableOpacity>
   );
 };
 
 export default Category;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  categoryText: {
+    fontSize: fontSize.md,
+    fontFamily: fontFamily.SemiBold,
+    color: colors.gray,
+  },
+});
