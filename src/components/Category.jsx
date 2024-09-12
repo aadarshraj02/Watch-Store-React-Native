a51eee954b6cdb78d1d7015d2de16e398ef44a8f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-12 19:31:29 +0530

    added border bottom

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,6 +1,6 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
-import {fontSize} from '../constants/dimensions';
+import {fontSize, spacing} from '../constants/dimensions';
 import {fontFamily} from '../constants/fonts';
 import {colors} from '../constants/colors';
 
@@ -8,6 +8,7 @@ const Category = () => {
   return (
     <TouchableOpacity>
       <Text style={styles.categoryText}>Smart Watch</Text>
+      <View style={styles.underline}></View>
     </TouchableOpacity>
   );
 };
@@ -19,5 +20,11 @@ const styles = StyleSheet.create({
     fontSize: fontSize.md,
     fontFamily: fontFamily.SemiBold,
     color: colors.gray,
+  },
+  underline: {
+    borderBottomColor: colors.purple,
+    borderBottomWidth: 2,
+    width: '70%',
+    marginTop: spacing.sm,
   },
 });
