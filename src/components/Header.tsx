4e4ec9df40c68df8cacd100a907489a18ff52bc5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-15 22:17:27 +0530

    added heart icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {iconSize} from '../constants/dimensions';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const Header = () => {
   return (
     <View>
-      <Ionicons name={'arrow-back'} size={iconSize.md} color="black" />
+      <TouchableOpacity>
+        <Ionicons name={'arrow-back'} size={iconSize.md} color="black" />
+        <AntDesign name={'hearto'} size={iconSize.md} color="black" />
+      </TouchableOpacity>
     </View>
   );
 };
