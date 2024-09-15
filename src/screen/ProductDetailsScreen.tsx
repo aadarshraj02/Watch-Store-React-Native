cc0a89f6b6b37e9d71b6accc8eae37186457807c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-15 22:26:00 +0530

    imported and added product component

--- src/screen/ProductDetailsScreen.tsx
+++ src/screen/ProductDetailsScreen.tsx
@@ -2,12 +2,14 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {useRoute} from '@react-navigation/native';
 import Header from '../components/Header';
+import ProductCarousel from '../components/ProductCarousel';
 
 const ProductDetailsScreen = (): JSX.Element => {
   const item = useRoute().params.item;
   return (
     <View>
       <Header />
+      <ProductCarousel />
     </View>
   );
 };
