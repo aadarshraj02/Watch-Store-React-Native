acf5197737eb76050a5ef2fc4535c994e4b6b551
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-15 22:27:40 +0530

    styled the container

--- src/screen/ProductDetailsScreen.tsx
+++ src/screen/ProductDetailsScreen.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import {useRoute} from '@react-navigation/native';
 import Header from '../components/Header';
 import ProductCarousel from '../components/ProductCarousel';
+import {spacing} from '../constants/dimensions';
+import {colors} from '../constants/colors';
 
 const ProductDetailsScreen = (): JSX.Element => {
   const item = useRoute().params.item;
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
       <ProductCarousel />
     </View>
@@ -16,4 +18,10 @@ const ProductDetailsScreen = (): JSX.Element => {
 
 export default ProductDetailsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: spacing.md,
+    backgroundColor: colors.background,
+  },
+});
