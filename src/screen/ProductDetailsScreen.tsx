19d87c1d8b1f935ed01606c59c2e69f203ded756
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-15 22:09:58 +0530

    displayed back icon

--- src/screen/ProductDetailsScreen.tsx
+++ src/screen/ProductDetailsScreen.tsx
@@ -1,11 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {useRoute} from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {iconSize} from '../constants/dimensions';
 
-const ProductDetailsScreen = () => {
+const ProductDetailsScreen = (): JSX.Element => {
   const item = useRoute().params.item;
   return (
     <View>
+      <Ionicons name={'arrow-back'} size={iconSize.md} color="black" />
       <Text>ProductDetailsScreen</Text>
     </View>
   );
