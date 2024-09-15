477506713334e45310ff659f6b66428b8f800998
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-15 22:24:45 +0530

    added functional export

---
+++ src/components/ProductCarousel.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProductCarousel = () => {
+  return (
+    <View>
+      <Text>ProductCarousel</Text>
+    </View>
+  )
+}
+
+export default ProductCarousel
+
+const styles = StyleSheet.create({})
