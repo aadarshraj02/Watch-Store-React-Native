8bd7e54d303db40ce00c6ee7641d22a53e7ba250
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-15 22:14:28 +0530

    added header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {iconSize} from '../constants/dimensions';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Header = () => {
   return (
     <View>
-      <Text>Header</Text>
+      <Ionicons name={'arrow-back'} size={iconSize.md} color="black" />
     </View>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screen/ProductDetailsScreen.tsx
+++ src/screen/ProductDetailsScreen.tsx
@@ -1,8 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {useRoute} from '@react-navigation/native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {iconSize} from '../constants/dimensions';
 import Header from '../components/Header';
 
 const ProductDetailsScreen = (): JSX.Element => {
