c0c1cb15bbdb96618448c30399aa194f523f4a4e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-15 21:22:23 +0530

    passed item

--- src/screen/ProductDetailsScreen.tsx
+++ src/screen/ProductDetailsScreen.tsx
@@ -1,7 +1,9 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useRoute} from '@react-navigation/native';
 
 const ProductDetailsScreen = () => {
+  const item = useRoute().params.item;
   return (
     <View>
       <Text>ProductDetailsScreen</Text>
