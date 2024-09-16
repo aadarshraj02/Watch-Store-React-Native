3d171ec550ba92367a5502ec1fd069e16f3332e8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-16 22:24:36 +0530

    wrapped in scrollView

--- src/screen/ProductDetailsScreen.tsx
+++ src/screen/ProductDetailsScreen.tsx
@@ -1,4 +1,11 @@
-import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {useState} from 'react';
 import {useRoute} from '@react-navigation/native';
 import Header from '../components/Header';
@@ -30,84 +37,89 @@ const ProductDetailsScreen = (): JSX.Element => {
   const item = useRoute().params.item;
   return (
     <View style={styles.container}>
-      <Header />
-      <ProductCarousel images={item.images} />
-      <View style={styles.titleContainer}>
-        <View style={styles.titleWrapper}>
-          <Text style={styles.productTitle}>{item.name}</Text>
-          <Text style={styles.productBrand}>{item.brand}</Text>
+      <ScrollView>
+        <Header />
+        <ProductCarousel images={item.images} />
+        <View style={styles.titleContainer}>
+          <View style={styles.titleWrapper}>
+            <Text style={styles.productTitle}>{item.name}</Text>
+            <Text style={styles.productBrand}>{item.brand}</Text>
+          </View>
+          <View style={styles.ratingWrapper}>
+            <AntDesign name={'star'} color={colors.yellow} size={iconSize.sm} />
+            <Text style={styles.productRating}>4.5</Text>
+          </View>
         </View>
-        <View style={styles.ratingWrapper}>
-          <AntDesign name={'star'} color={colors.yellow} size={iconSize.sm} />
-          <Text style={styles.productRating}>4.5</Text>
-        </View>
-      </View>
-      <View style={styles.colorsContainer}>
-        <Text style={styles.colorsHeading}>Colors</Text>
-        <FlatList
-          data={colorsData}
-          renderItem={({item, index}) => (
-            <TouchableOpacity
-              key={index}
-              style={[
-                styles.selectColorContainer,
-                item.colorValue === selectedColor && {
-                  borderColor: colors.purple,
-                },
-              ]}
-              onPress={() => {
-                setSelectedColor(item.colorValue);
-              }}>
+        <View style={styles.colorsContainer}>
+          <Text style={styles.colorsHeading}>Colors</Text>
+          <FlatList
+            data={colorsData}
+            renderItem={({item, index}) => (
+              <TouchableOpacity
+                key={index}
+                style={[
+                  styles.selectColorContainer,
+                  item.colorValue === selectedColor && {
+                    borderColor: colors.purple,
+                  },
+                ]}
+                onPress={() => {
+                  setSelectedColor(item.colorValue);
+                }}>
+                <View
+                  style={[
+                    styles.circleColor,
+                    {backgroundColor: item.colorValue},
+                  ]}
+                />
+                <Text style={styles.colorText}>{item.colorName}</Text>
+              </TouchableOpacity>
+            )}
+            horizontal
+            ItemSeparatorComponent={() => (
               <View
-                style={[styles.circleColor, {backgroundColor: item.colorValue}]}
-              />
-              <Text style={styles.colorText}>{item.colorName}</Text>
-            </TouchableOpacity>
-          )}
-          horizontal
-          ItemSeparatorComponent={() => (
-            <View
-              style={{
-                width: spacing.sm,
-              }}></View>
-          )}
-        />
-      </View>
-      <View style={styles.detailsReviewTabs}>
-        <TouchableOpacity
-          onPress={() => {
-            setSelectedTab('Details');
-          }}>
-          <Text
-            style={[
-              styles.tabText,
-              selectedTab === 'Details' && {
-                color: colors.purple,
-              },
-            ]}>
-            Details
-          </Text>
-          {selectedTab === 'Details' && <View style={styles.underline} />}
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            setSelectedTab('Review');
-          }}>
-          <Text
-            style={[
-              styles.tabText,
-              selectedTab === 'Review' && {
-                color: colors.purple,
-              },
-            ]}>
-            Review
-          </Text>
-          {selectedTab === 'Review' && <View style={styles.underline} />}
-        </TouchableOpacity>
-      </View>
-      <Text style={styles.detailsContainer}>
-        {selectedTab === 'Details' ? item.details : item.review}
-      </Text>
+                style={{
+                  width: spacing.sm,
+                }}></View>
+            )}
+          />
+        </View>
+        <View style={styles.detailsReviewTabs}>
+          <TouchableOpacity
+            onPress={() => {
+              setSelectedTab('Details');
+            }}>
+            <Text
+              style={[
+                styles.tabText,
+                selectedTab === 'Details' && {
+                  color: colors.purple,
+                },
+              ]}>
+              Details
+            </Text>
+            {selectedTab === 'Details' && <View style={styles.underline} />}
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              setSelectedTab('Review');
+            }}>
+            <Text
+              style={[
+                styles.tabText,
+                selectedTab === 'Review' && {
+                  color: colors.purple,
+                },
+              ]}>
+              Review
+            </Text>
+            {selectedTab === 'Review' && <View style={styles.underline} />}
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.detailsContainer}>
+          {selectedTab === 'Details' ? item.details : item.review}
+        </Text>
+      </ScrollView>
     </View>
   );
 };
